d052c7982367b3c2068aaa80d88c07bddf7f132b
bruski <292803281@qq.com>
2020-10-19 10:04:23 +0800

    refactor(button): 重构样式名

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-// import PreviewButton from './previews/PreviewButton'
-// import PreviewAlert from './previews/PreviewAlert'
+import PreviewButton from './previews/PreviewButton'
+import PreviewAlert from './previews/PreviewAlert'
 import PreviewMenu from './previews/PreviewMenu'
 
 function App() {
   return (
     <div>
+      <PreviewButton />
+      <hr/>
+      <PreviewAlert />
+      <hr/>
       <PreviewMenu />
     </div>
   );
