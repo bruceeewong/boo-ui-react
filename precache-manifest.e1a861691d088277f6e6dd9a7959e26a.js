b9f4258f04dc32a6c0f425c0f18f88efbc83a696
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-20 16:07:08 +0000

    Deploy bruceeewong/boo-ui-react to github.com/bruceeewong/boo-ui-react.git:gh-pages

---
+++ precache-manifest.e1a861691d088277f6e6dd9a7959e26a.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2db8055bbe9f835780f0599bca93a23",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "e1c28c7a87a3d876a084",
+    "url": "main.css"
+  },
+  {
+    "url": "main.d7170ad5c984ad61d957.bundle.js"
+  },
+  {
+    "url": "runtime~main.d7170ad5c984ad61d957.bundle.js"
+  },
+  {
+    "revision": "b8291cbccbdd2c097a69",
+    "url": "vendors~main.css"
+  },
+  {
+    "url": "vendors~main.d7170ad5c984ad61d957.bundle.js"
+  }
+]);
